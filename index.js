0c59d328fbdaf366e5616c79f3fbb974e3beb626
Jeova Junior <jrjeova@hotmail.com>
2024-05-09 12:01:23 -0300

    App porta

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = require('./src/routes');
 const app = express();
-const port = 3000;
+const port = process.env.APP_PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
