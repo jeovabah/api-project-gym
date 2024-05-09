c0cdfabb711637347a421200d5f2b57023eff037
Jeova Junior <jrjeova@hotmail.com>
2024-05-09 15:59:05 -0300

    Origin todos

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use('/', routes);
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 app.listen(PORT,()=>{
     console.log(`Iniciado na porta ${PORT}`);
 })
