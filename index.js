16cadc89e783501cf5b86e4491d2e45d8fbe49cf
Jeova Junior <jrjeova@hotmail.com>
2024-05-09 12:23:36 -0300

    porta

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const routes = require('./src/routes');
 const app = express();
-const port = 3000;
-
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use('/', routes);
 
-app.listen(port,()=>{
-    console.log(`Iniciado na porta ${port}`);
+app.listen(PORT,()=>{
+    console.log(`Iniciado na porta ${PORT}`);
 })
