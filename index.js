6481149894417a2c895dc9d2863248c883fa21be
Jeova Junior <jrjeova@hotmail.com>
2024-05-09 15:36:39 -0300

    Ajustando cors

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const routes = require('./src/routes');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use('/', routes);
-
+app.use(cors());
 app.listen(PORT,()=>{
     console.log(`Iniciado na porta ${PORT}`);
 })
