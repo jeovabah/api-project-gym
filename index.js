c6629a814f0e5ba574e5523cb2764e872a68ad14
Jeova Junior <jrjeova@hotmail.com>
2024-05-09 16:43:43 -0300

    Use cors

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const routes = require('./src/routes');
 const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
-app.use('/', routes);
 app.use(cors({
     origin: "*"
 }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use('/', routes);
+
 app.listen(PORT,()=>{
     console.log(`Iniciado na porta ${PORT}`);
 })
